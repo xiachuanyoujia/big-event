56ef5e1c21277664ade85efebecbdabf1ffb8bb7
xiachuanyoujia <1243357202@qq.com>
2023-11-27 21:45:55 +0800

    refactor：注册接口

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -22,14 +22,13 @@ public class UserController {
     public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
         //查询用户
         User u = userService.findByUserName(username);
-        if (u == null) {
-            //没有占用
-            //注册
-            userService.register(username, password);
-            return Result.success();
-        } else {
-            //占用
+        //占用
+        if (u != null) {
             return Result.error("用户名被占用！");
         }
+        //没有占用
+        //注册
+        userService.register(username, password);
+        return Result.success();
     }
 }
