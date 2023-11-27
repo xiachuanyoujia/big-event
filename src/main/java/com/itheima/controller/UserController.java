75ebec8af2451d552494f210356fd8cec5e1922f
xiachuanyoujia <1243357202@qq.com>
2023-11-27 21:54:56 +0800

    feat：登录接口主要逻辑

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -3,6 +3,7 @@ package com.itheima.controller;
 import com.itheima.pojo.Result;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
+import com.itheima.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -30,5 +31,17 @@ public class UserController {
         //注册
         userService.register(username, password);
         return Result.success();
+    }
+
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
+        User loginUser = userService.findByUserName(username);
+        if (loginUser == null) {
+            return Result.error("用户名错误");
+        }
+        if (Md5Util.getMD5String(password).equals(loginUser.getPassword())) {
+            return Result.success("jwt令牌。。。");
+        }
+        return Result.error("密码错误");
     }
 }
