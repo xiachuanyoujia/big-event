81537dcb228cae7d480425954669907f33c31ab1
xiachuanyoujia <1243357202@qq.com>
2023-12-19 11:26:44 +0800

    fix：分组校验

--- src/main/java/com/itheima/pojo/Category.java
+++ src/main/java/com/itheima/pojo/Category.java
@@ -3,12 +3,14 @@ package com.itheima.pojo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+
 @Data
 public class Category {
-    @NotNull
+    @NotNull(groups = Update.class)
     private Integer id;//主键ID
     @NotEmpty
     private String categoryName;//分类名称
@@ -19,4 +21,15 @@ public class Category {
     private LocalDateTime createTime;//创建时间
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;//更新时间
+
+    //如果说菜个校验项没有指定分组,默认属于Default分组
+    //分组之问可以继承，A extends B 那么A中拥有B中所有的校验项
+
+    public interface Add extends Default {
+
+    }
+
+    public interface Update extends Default {
+
+    }
 }
